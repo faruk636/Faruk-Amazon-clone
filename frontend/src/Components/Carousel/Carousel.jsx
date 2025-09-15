54c8941a58329fe63096402d439b1d044bedfff2
Faruk Ahmed <far12345ahmed@gmail.com>
2025-09-15 21:35:03 +0300

    carousel

---
+++ frontend/src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,25 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {img} from './img/data'
+import classes from './carousel.module.css'
+
+const CarouselEffect = () => {
+  return (
+    <>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+      >
+        {img.map((imgItem) => {
+          return <img src={imgItem} alt="carousel" />;
+        })}
+      </Carousel>
+      <div className={classes.hero__img}></div>
+      {/* { console.log(img)} */}
+    </>
+  );
+};
+
+export default CarouselEffect;
--- frontend/src/Pages/Landing/Landing.jsx
+++ frontend/src/Pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 
+import CarouselEffect from '../../Components/Carousel/Carousel'
 import Header from '../../Components/Header/Header'
 
 const Landing = () => {
   return (
     <>
      <Header/>
+     <CarouselEffect/>
     </>
   )
 }
